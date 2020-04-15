c46e4c46686b227f4d915e290c3a1bb1326cf7c0
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2020-04-15 19:34:53 +0800

    django added

--- 7.django/djangoair/flights/models.py
+++ 7.django/djangoair/flights/models.py
@@ -15,3 +15,11 @@ class Flight(models.Model):
 
   def __str__(self):
     return f"{self.id} - {self.origin} to {self.destination}, {self.duration}"
+
+class Passenger(models.Model):
+  firstname = models.CharField(max_length=64)
+  lastname = models.CharField(max_length=64)
+  flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
