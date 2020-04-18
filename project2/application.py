5a922faed61896316521213d22454773c8403eae
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2020-04-18 20:39:17 +0800

    user login

---
+++ project2/application.py
@@ -0,0 +1,42 @@
+import os
+import requests
+
+from flask import Flask, render_template, request, redirect, jsonify, session
+from flask_socketio import SocketIO, emit
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+names = []
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+@app.route("/get_names")
+def get_names():
+  print(names)
+  return jsonify(names)
+
+@socketio.on("add name")
+def add(data):
+    username = data["username"]
+    names.append(username)
+    emit("announce names", {"username": username}, broadcast=True)  
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  socketio.run(app)
